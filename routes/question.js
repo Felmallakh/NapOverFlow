1b1b3158add7e69ae655a2b424a0154d7db91b12
Geoffrey Cox <geoffrey.pt.cox@gmail.com>
2021-11-16 07:13:09 -1000

    got the GET route to work for the form

--- routes/question.js
+++ routes/question.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 
 const { asyncHandler, csrfProtection } = require("./utils");
+const { Question } = require('../db/models');
+const { requireAuth } = require('../auth');
 
 const router = express.Router();
 
-router.get('/new', csrfProtection, (req, res) => {
-    res.render('new-question', { csrfToken: req.csrfToken(), create: {} });
+router.get('/new', requireAuth, csrfProtection, (req, res) => {
+    res.render('new-question', { csrfToken: req.csrfToken(), title: 'Ask Question', question: {} });
 });
 
 module.exports = router;
